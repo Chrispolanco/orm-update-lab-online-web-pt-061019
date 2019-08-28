144fbd652c3280a531180c36031fec986ebd57f6
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-28 02:18:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  def initialize(id=nil, name, grade)
+  def initialize(id=nil, :name, grade)
     @id = id
     @name = name
     @grade = grade 
