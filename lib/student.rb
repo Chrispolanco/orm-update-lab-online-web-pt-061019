4d237d82ccb80d6fa4a6169721c6adc28bc3a05e
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-28 02:25:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,14 @@ attr_reader :id
 
     DB[:conn].execute(sql)
   end
+
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
+  end
+  
   
   def update
     sql = "UPDATE songs SET name = ?, grade = ? WHERE id = ?"
