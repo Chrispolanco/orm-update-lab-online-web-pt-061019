bfeace813ea8dc7319e1b84e9759670cfdb66ecf
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-28 02:30:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ attr_reader :id
 
   def self.create_table
     sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS student (
+      CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade TEXT
@@ -25,7 +25,7 @@ attr_reader :id
 
   
   def self.drop_table
-    sql = "DROP TABLE IF EXISTS student"
+    sql = "DROP TABLE IF EXISTS students"
 
     DB[:conn].execute(sql)
   end
@@ -49,11 +49,11 @@ attr_reader :id
       self.update
     else
       sql = <<-SQL
-        INSERT INTO student (name, grade)
+        INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM student")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
 
